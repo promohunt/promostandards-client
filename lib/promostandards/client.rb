3d1c46922c5b6054e35524ad4d31bf992ec47610
Akshay S Murthy <iamakshay@gmail.com>
2018-10-05 16:13:15 +0530

    Get rid of PromoStandards::SOAP::Client
    Unnecessary indirection

--- lib/promostandards/client.rb
+++ lib/promostandards/client.rb
@@ -1,44 +1,97 @@
-require_relative 'soap/client'
 require_relative 'meta_api/client'
 require 'promostandards/client/version'
+require 'savon'
 
 module PromoStandards
   class Client
+    COMMON_SAVON_CLIENT_CONFIG = {
+      # pretty_print_xml: true,
+      # log: true,
+      env_namespace: :soapenv,
+      namespace_identifier: :ns
+    }
+
     def initialize(access_id:, password:, product_data_service_url:, media_content_service_url:)
-      @soap_client = build_soap_client(access_id, password)
+      @access_id = access_id
+      @password = password
       @product_data_service_url = product_data_service_url
       @media_content_service_url = media_content_service_url
     end
 
     def get_sellable_product_ids
-      @soap_client.get_sellable_product_ids(
-        product_data_service_url: @product_data_service_url
-      ).map do |minimal_product|
-        minimal_product[:product_id]
-      end
+      client = build_savon_client_for_product(@product_data_service_url)
+      response = client.call('GetProductSellableRequest',
+        message: {
+          'shar:wsVersion' => '1.0.0',
+          'shar:id' => @access_id,
+          'shar:password' => @password,
+          'shar:isSellable' => true
+        },
+        soap_action: 'getProductSellable'
+      )
+      response.body[:get_product_sellable_response][:product_sellable_array][:product_sellable]
+        .map do |minimal_product|
+          minimal_product[:product_id]
+        end
     end
 
     def get_product_data(product_id)
-      @soap_client.get_product(
-        product_data_service_url: @product_data_service_url,
-        product_id: product_id
+      client = build_savon_client_for_product(@product_data_service_url)
+      response = client.call('GetProductRequest',
+        message: {
+          'shar:wsVersion' => '1.0.0',
+          'shar:id' => @access_id,
+          'shar:password' => @password,
+          'shar:localizationCountry' => 'US',
+          'shar:localizationLanguage' => 'en',
+          'shar:productId' => product_id,
+        },
+        soap_action: 'getProduct'
       )
+      response.body[:get_product_response][:product]
     end
 
     def get_primary_image(product_id)
-      @soap_client.get_primary_image(
-        media_content_service_url: @media_content_service_url,
-        product_id: product_id
+      client = build_savon_client_for_media(@media_content_service_url)
+      response = client.call('GetMediaContentRequest',
+        message: {
+          'shar:wsVersion' => '1.1.0',
+          'shar:id' => @access_id,
+          'shar:password' => @password,
+          'shar:mediaType' => 'Image',
+          'shar:productId' => product_id,
+          'ns:classType' => '1006'
+        },
+        soap_action: 'getMediaContent'
       )
+      if response.body[:get_media_content_response][:media_content_array][:media_content].is_a? Array
+        response.body[:get_media_content_response][:media_content_array][:media_content].first
+      else
+        response.body[:get_media_content_response][:media_content_array][:media_content]
+      end
     end
 
     private
 
-    def build_soap_client(access_id, password)
-      PromoStandards::SOAP::Client.new(
-        access_id: access_id,
-        password: password
+    def build_savon_client_for_product(service_url)
+      Savon.client COMMON_SAVON_CLIENT_CONFIG.merge(
+        endpoint: service_url,
+        namespace: 'http://www.promostandards.org/WSDL/ProductDataService/1.0.0/',
+        namespaces: {
+          'xmlns:shar' => 'http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/'
+        }
       )
     end
+
+    def build_savon_client_for_media(service_url)
+      Savon.client COMMON_SAVON_CLIENT_CONFIG.merge(
+        endpoint: service_url,
+        namespace: 'http://www.promostandards.org/WSDL/MediaService/1.0.0/',
+        namespaces: {
+          'xmlns:shar' => 'http://www.promostandards.org/WSDL/MediaService/1.0.0/SharedObjects/'
+        }
+      )
+    end
+
   end
 end
