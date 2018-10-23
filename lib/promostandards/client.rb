ad432788464a6d455e6533db10a850c16ea606c2
Amit Choudhary <amitchoudhary1008@gmail.com>
2018-10-23 16:38:36 +0200

    (fix) - fix condition when class_type is array

--- lib/promostandards/client.rb
+++ lib/promostandards/client.rb
@@ -79,10 +79,16 @@ module PromoStandards
           primary_media_content = media_content.find do |media|
             class_type_array = media[:class_type_array]
 
-            if class_type_array.is_a?(Hash)
+            if class_type_array.is_a?(Hash) && image_precendence_number.is_a?(String)
               [media[:class_type_array][:class_type][:class_type_id]].include?(image_precendence_number)
             elsif class_type_array.is_a?(Array)
-              [media[:class_type_array]].map { |item| item[:class_type][:class_type_id] }.include?(image_precendence_number)
+              class_type_ids = media[:class_type_array].map { |item| item[:class_type][:class_type_id] }
+
+              if image_precendence_number.is_a?(Array)
+                (class_type_ids & image_precendence_number).any?
+              else
+                class_type_ids.include?(image_precendence_number)
+              end
             end
           end
         end
