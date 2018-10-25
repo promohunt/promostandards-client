5df1b997270f02b8bb969eb88aa2d7502dd575ed
Amit Choudhary <amitchoudhary1008@gmail.com>
2018-10-25 10:11:16 +0200

    (test) - fix tests for Client and handled a case when media[:class_type_array] is not present

--- lib/promostandards/client.rb
+++ lib/promostandards/client.rb
@@ -77,20 +77,22 @@ module PromoStandards
 
         PRIMARY_IMAGE_PRECEDENCE.find do |image_precendence_number|
           primary_media_content = media_content.find do |media|
-            class_type_array = media[:class_type_array][:class_type]
+            if media[:class_type_array]
+              class_type_array = media[:class_type_array][:class_type]
 
-            class_type_ids = []
+              class_type_ids = []
 
-            if class_type_array.is_a?(Hash)
-              class_type_ids = [class_type_array[:class_type_id]]
-            elsif class_type_array.is_a?(Array)
-              class_type_ids = class_type_array.map { |item| item[:class_type_id] }
-            end
+              if class_type_array.is_a?(Hash)
+                class_type_ids = [class_type_array[:class_type_id]]
+              elsif class_type_array.is_a?(Array)
+                class_type_ids = class_type_array.map { |item| item[:class_type_id] }
+              end
 
-            if image_precendence_number.is_a?(Array)
-              (class_type_ids & image_precendence_number).any?
-            else
-              class_type_ids.include?(image_precendence_number)
+              if image_precendence_number.is_a?(Array)
+                (class_type_ids & image_precendence_number).any?
+              else
+                class_type_ids.include?(image_precendence_number)
+              end
             end
           end
         end
